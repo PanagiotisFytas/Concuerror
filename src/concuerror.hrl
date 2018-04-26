e67709810b1531ce8cf7bef0bd0c05947fc9e8ba
PanagiotisFytas <pfytas@gmail.com>
2018-04-26 12:52:59 +0300

    Avoid infinite loop in case epmd cannot start

--- src/concuerror.hrl
+++ src/concuerror.hrl
@@ -176,6 +176,7 @@
               ['$1']}]))).
 %%------------------------------------------------------------------------------
 %% for parallel mode
+-define(epmd_tries, 10).
 -define(epmd_not_running_response, "epmd: Cannot connect to local epmd\n").
 %%------------------------------------------------------------------------------
 -type links() :: ets:tid().
